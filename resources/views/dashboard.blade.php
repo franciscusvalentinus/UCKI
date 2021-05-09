17cbc0712d6389e04c2414e97ff244345c7eb408
Franciscus Valentinus Ongkosianbhadra <fvalentinus@student.ciputra.ac.id>
2021-05-09 09:27:05 +0700

    view dashboard horizontal line

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg divide-y divide-gray-400 md:divide-y-8">
                 <div id="wrapper" class="max-w-xl px-4 py-4 mx-auto">
                     <form action="{{ url()->current() }}" method="get">
                         <div class="relative mx-auto">
