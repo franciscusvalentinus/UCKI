1ab09c2d217b03680829ec0dc1bd4af9c3af9075
Franciscus Valentinus Ongkosianbhadra <fvalentinus@student.ciputra.ac.id>
2021-05-10 15:19:17 +0700

    view rename header to bahasa indonesia

--- resources/views/creations/index.blade.php
+++ resources/views/creations/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Creations List
+            Jenis Karya Cipta
         </h2>
     </x-slot>
 
--- resources/views/patents/index.blade.php
+++ resources/views/patents/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Patents List
+            Paten
         </h2>
     </x-slot>
 
